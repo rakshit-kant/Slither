136b9b521e590f6102226f40a3591f8938a15738
rakshit-kant <rakshitkant100@gmail.com>
2026-07-30 20:10:20 +0530

    Implemented the Vector for the Snake

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+struct Game {
+    snake: Vec<(usize, usize)>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let game = Game {
+        snake: vec![(5, 5), (4, 5), (3, 5)],
+    };
+
+    println!("{:?}", game.snake);
 }
